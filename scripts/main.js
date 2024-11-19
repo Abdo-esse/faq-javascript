6c3638272fbd0a203d4040a665dcb2f6422a24af
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-19 15:11:26 +0100

    J'ai termin la modification sur la function addNewQuestion

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,97 @@
-
 //Question text input
-const questionText = document.getElementById('question');
+const questionText = document.getElementById("question");
 
 //Question answer input
-const correctAnswer = document.getElementById('answer');
+const correctAnswer = document.getElementById("answer");
 
 //Question count div
 const questionCountDiv = document.getElementById("questionCount");
 
 //Add new question/answer form
-const form = document.getElementById('addQuestionForm');
+const form = document.getElementById("addQuestionForm");
 
 //count Id
 let countId = 6;
 
-
 function addNewQuestion() {
-   
-  
-    // Function body to be implemented 
-    form.addEventListener('submit', (e) => {
-       
-        
-       e.preventDefault()
-       
-       
-       
-       countId++
-       let data={
-       id:countId,
-       status:"open",
-       question: questionText.value,
-       answer:  correctAnswer.value
-   }
-   console.log(data);
-      faqData.push(data)
-   console.log(faqData);
+  // Function body to be implemented
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const radios = document.querySelectorAll('input[name="staus"]');
+    let selectedValue = null;
 
-   renderFAQ(faqData)
-   displayQuestionCount()
+    radios.forEach((radio) => {
+      if (radio.checked) {
+        selectedValue = radio.value;
+        console.log(selectedValue);
+        
+      }
     });
-    
+
+    countId++;
+    var data = {
+      id: countId,
+      status:  selectedValue,
+      question: questionText.value,
+      answer: correctAnswer.value,
+    };
+    console.log(data);
+    faqData.push(data);
+    console.log(faqData);
+
+    // //    function checkRadio(value) {
+    // //     if (value == "open") {
+    // //         data.status=value
+    // //       console.log("Choice: ", value);
+    // //       document.getElementById("openRadio").checked = true;
+    // //       document.getElementById("closedRadio").checked = false;
+
+    // //     } else if (value == "closed") {
+    // //         data.status=value
+    // //       console.log("Choice: ", value);
+    // //       document.getElementById("closedRadio").checked = true;
+    // //       document.getElementById("openRadio").checked = false;
+    // //     }
+    //   }
+    renderFAQ(faqData);
+    displayQuestionCount();
+  });
 }
 
-addNewQuestion()
+addNewQuestion();
 console.log(faqData);
 
 function displayQuestionCount() {
-    // Function body to be implemented
-    console.log('hdfff');
-    
-  let  questionCount = faqData.filter(item=>item.question!==''&item.answer!==''&item.status=="open")
-  questionCountDiv.textContent=questionCount.length
-   
-    
+  // Function body to be implemented
+  console.log("hdfff");
+
+  let questionCount = faqData.filter(
+    (item) =>
+      (item.question !== "") & (item.answer !== "") & (item.status == "open")
+  );
+  questionCountDiv.textContent = questionCount.length;
 }
-displayQuestionCount()
+displayQuestionCount();
 
 function initializeQuestionList() {
-    // Function body to be implemented
-
+  // Function body to be implemented
 }
-
-
-
 
 function deleteQuestion(questionId) {
-    // Function body to be implemented
-    
-    
+  // Function body to be implemented
 }
 
+// function checkRadio(value) {
+//     if (value == "open") {
+//         data.status=value
+//       console.log("Choice: ", value);
+//       document.getElementById("openRadio").checked = true;
+//       document.getElementById("closedRadio").checked = false;
 
-
+//     } else if (value == "closed") {
+//         data.status=value
+//       console.log("Choice: ", value);
+//       document.getElementById("closedRadio").checked = true;
+//       document.getElementById("openRadio").checked = false;
+//     }
+//   }
